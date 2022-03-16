4368ea016afce0a32719bfd3e798a540a4bb39c2
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-17 00:22:46 +0100

    commit

--- src/components/backOffice/BackOffice.tsx
+++ src/components/backOffice/BackOffice.tsx
@@ -6,8 +6,8 @@ import './styles.scss'
 export default function BackOffice() {
   return (
     <RB.Row id='bo-container'>
-      <h2 className='text-center mb-5'>BackOffice</h2>
-      <h5 className='text-center mb-3'>Add a new product to the catalogue</h5>
+      <h2 className='t-BO mb-5'>BackOffice</h2>
+      <h5 className='t-addNewProd mb-3'>Add a new product to the catalogue</h5>
       <RB.Col md={6}>
         <Form />
       </RB.Col>
