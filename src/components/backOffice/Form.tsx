6ddee0e7a06004fc159532b1f8ab3c85241ea530
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-17 15:41:15 +0100

    commit

--- src/components/backOffice/Form.tsx
+++ src/components/backOffice/Form.tsx
@@ -1,9 +1,10 @@
 import * as RB from 'react-bootstrap'
 import { useState } from 'react'
-import { API, graphqlOperation } from 'aws-amplify'
+import { Storage, API, graphqlOperation } from 'aws-amplify'
 import { useRecoilState } from 'recoil'
 import { productState } from '../atoms'
 import { createProduct } from '../../graphql/mutations'
+import awsExports from '../../aws-exports'
 import './styles.scss'
 
 export default function Form() {
@@ -35,6 +36,31 @@ export default function Form() {
         }
     }
 
+    console.log('Main Image', newProduct.image);
+
+    const target = (e: any) => {
+        if (e.target && e.target.files[0]) {
+            const file = e.target.files[0]
+            console.log('Here is the image', file)
+
+            Storage.put(file.name, file, {
+                contentType: 'image/png|image/jpeg'
+            }).then((response) => {
+                updateNewProduct({ ...newProduct, image: URL.createObjectURL(file) })
+                console.log(response)
+                const image = {
+                    name: file.name,
+                    file: {
+                        bucket: awsExports.aws_user_files_s3_bucket,
+                        region: awsExports.aws_user_files_s3_bucket_region,
+                        public: 'public/' + file.name
+                    }
+                }
+                console.log('New image', image)
+            }).catch(err => console.log(err))
+        }
+    }
+
     return (
         <RB.Form id='form'>
             <RB.FormGroup>
@@ -56,9 +82,8 @@ export default function Form() {
             <RB.FormGroup>
                 <RB.FormControl
                     className='formControl'
-                    value={newProduct.image}
-                    type='text'
-                    onChange={(e) => updateInput('image', e.target.value)}
+                    type='file'
+                    onChange={(e) => target(e)}
                     placeholder='image url' />
             </RB.FormGroup>
             <RB.FormGroup className='position-relative'>
