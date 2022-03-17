4a0c6e3300aec78608eb8966bcf4072cc5e782a2
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-17 12:00:45 +0100

    commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "media": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
