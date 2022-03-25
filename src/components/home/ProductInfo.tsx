0e08fd4ac7438e50b8b0cd494c3fbfd2ed477ac8
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-25 12:11:01 +0100

    commit

--- src/components/home/ProductInfo.tsx
+++ src/components/home/ProductInfo.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Product } from '../atoms'
 
 export default function ProductInfo() {
     const { id } = useParams()
     console.log('This is the id of the product', id)
-  return (
-    <div>
 
-    </div>
-  )
+    const [product, updateProduct] = useState<Product>({})
+
+    async function getProduct() {
+        try {
+            const { data }: any = await API.graphql(graphqlOperation(getProduct, { id: id }))
+            const { items } = data.listProducts
+            updateProduct(items)
+        } catch (error) {
+            console.log('Error getting product from DB', error)
+        }
+    }
+
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+    return (
+        <div>
+            <h1 className="text-dark">{product.name}</h1>
+        </div>
+    )
 }
