115503afbebf5bb033794db01c15b8d6f2ae64ac
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-25 11:20:43 +0100

    commit

--- src/components/home/ProductInfo.tsx
+++ src/components/home/ProductInfo.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ProductInfo() {
   return (
-    <div>ProductInfo</div>
+    <div>
+        
+    </div>
   )
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import Home from './components/home/Home';
 import BackOffice from './components/backOffice/BackOffice';
+import Product from './components/home/ProductInfo';
 
 export const routes: Array<[string, React.FC]> = [
     ["/", Home],
     ["/backoffice", BackOffice],
+    ["/:id/ProductInfo", BackOffice]
  
 ]
