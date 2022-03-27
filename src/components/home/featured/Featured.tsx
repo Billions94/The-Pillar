ece9bcad2db98cb2749e950ad9fdb03385a9e456
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-27 20:01:32 +0200

    commit

--- src/components/home/featured/Featured.tsx
+++ src/components/home/featured/Featured.tsx
@@ -14,7 +14,21 @@ export default function Featured () {
                 <Col sm={12} md={2} lg={2}>
                     <h1 className='t-header text-left'>Featured</h1>
                 </Col>
-                <Col sm={12} md={10} lg={20}>
+                <Col className='featured-container' sm={12} md={10} lg={20}>
+                    <Row className='featured-grid'>
+                        <Col className='top-left' sm={12} md={6} lg={6}>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint assumenda repellat expedita eveniet dolor temporibus aspernatur velit consequatur iste? Culpa optio maiores necessitatibus aperiam corporis asperiores laborum minima, fugit nam?
+                        </Col>
+                        <Col className='top-right' sm={12} md={6} lg={6}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
+                        </Col>
+                        <Col className='bottom-left' sm={12} md={6} lg={6}>
+                            lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
+                        </Col>
+                        <Col className='bottom-right' sm={12} md={6} lg={6}>
+                            lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
