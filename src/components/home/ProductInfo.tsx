def17a86f90ad62bf1d5de2663f6d0e1463e3626
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-27 18:51:47 +0200

    commit

--- src/components/home/ProductInfo.tsx
+++ src/components/home/ProductInfo.tsx
@@ -5,19 +5,18 @@ import { useParams } from 'react-router-dom'
 import { getProduct } from '../../graphql/queries'
 import { darkModeState, Product } from '../atoms'
 import { useRecoilValue } from 'recoil'
+import Carousel from './carousel/Carousel'
 
 export default function ProductInfo() {
-    const { id } = useParams()
-    console.log('This is the id of the product', id)
 
+    const { id } = useParams()
     const [product, updateProduct] = useState<Product>({})
     const darkMode = useRecoilValue(darkModeState)
-    const check: boolean = darkMode === false 
+    const check: boolean = darkMode === false
 
     async function getProductById() {
         try {
             const { data }: any = await API.graphql(graphqlOperation(getProduct, { id: id }))
-            console.log('This is the data from the product', data)
             const item = data.getProduct
             updateProduct(item)
         } catch (error) {
@@ -30,22 +29,23 @@ export default function ProductInfo() {
     }, [])
 
     return (
-        <div>
-            <div className={check ? 'card-holder' : 'card-holderDark'}>
-                <RB.Card onClick={() => toggle(idx)}
-                    className='card'>
-                    <RB.Card.Header className='card-header'
-                        style={{ width: '186px', height: '155px' }}>
-                        <RB.Image className='card-img' src={item.image} alt='' />
-                    </RB.Card.Header>
-                    <RB.Card.Body className='card-body'>
-                        <h6 className='card-text'>{item.name}</h6>
-                        <h6 className='card-text'>{item.description}</h6>
-                        <h6 className='card-text'>{item.price}</h6>
-                        <h6 className='card-text'>{item.category}</h6>
-                    </RB.Card.Body>
-                </RB.Card>
-            </div>
-        </div>
+        <>
+            <section id='product'>
+                <RB.Row id={check ? 'product' : 'productDark'} className='p-4'>
+                    <RB.Col className='product-info' md={10} lg={10}>
+                        <div className='card-container'>
+                            <h1 className='title mb-2'>{product.name}</h1>
+                            <RB.Image src={product.image} alt='' className='img'/>
+                            {/* <Carousel image={product.image} /> */}
+                        </div>
+                    </RB.Col>
+                </RB.Row>
+
+            </section>
+
+            <section>
+
+            </section>
+        </>
     )
 }
---
+++ src/components/home/carousel/Carousel.tsx
@@ -0,0 +1,48 @@
+import * as RB from 'react-bootstrap'
+import './styles.scss'
+
+interface CarouselProps {
+    image?: string
+}
+
+export default function Carousel({ image }: CarouselProps) {
+    return (
+        <RB.Carousel id='carousel'>
+            <RB.Carousel.Item>
+                <img
+                    className="d-block img"
+                    src={image}
+                    alt="First slide"
+                />
+                <RB.Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </RB.Carousel.Caption>
+            </RB.Carousel.Item>
+            {/* <RB.Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Second slide&bg=282c34"
+                    alt="Second slide"
+                />
+
+                <RB.Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </RB.Carousel.Caption>
+            </RB.Carousel.Item>
+            <RB.Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Third slide&bg=20232a"
+                    alt="Third slide"
+                />
+
+                <RB.Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </RB.Carousel.Caption>
+            </RB.Carousel.Item> */}
+        </RB.Carousel>
+    )
+}
