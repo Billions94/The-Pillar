9f93745fe59088b8ef343199ec4d67866ebffc7d
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-27 20:42:20 +0200

    commit

--- src/components/home/featured/Featured.tsx
+++ src/components/home/featured/Featured.tsx
@@ -3,7 +3,7 @@ import { useRecoilValue } from 'recoil'
 import { darkModeState } from '../../atoms'
 import './styles.scss'
 
-export default function Featured () {
+export default function Featured() {
 
     const darkMode = useRecoilValue(darkModeState)
     const check: boolean = darkMode === false
@@ -17,15 +17,19 @@ export default function Featured () {
                 <Col className='featured-container' sm={12} md={10} lg={20}>
                     <Row className='featured-grid'>
                         <Col className='top-left' sm={12} md={6} lg={6}>
+                            <h1 className='Nig '>Nigerian History</h1>
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint assumenda repellat expedita eveniet dolor temporibus aspernatur velit consequatur iste? Culpa optio maiores necessitatibus aperiam corporis asperiores laborum minima, fugit nam?
                         </Col>
                         <Col className='top-right' sm={12} md={6} lg={6}>
+                            <h1 className='Gh'>Ghanaian History</h1>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
                         </Col>
                         <Col className='bottom-left' sm={12} md={6} lg={6}>
+                            <h1 className='Jac'>Jamaican History</h1>
                             lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
                         </Col>
                         <Col className='bottom-right' sm={12} md={6} lg={6}>
+                            <h1 className='Sa'>South African History</h1>
                             lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nisi numquam aspernatur autem delectus? Sapiente quam non, aut, rerum corrupti ea similique totam quas vel repudiandae culpa incidunt architecto perspiciatis.
                         </Col>
                     </Row>
