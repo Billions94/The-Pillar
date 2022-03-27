789f403fa3175e5088cd0d439966577d70107003
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-27 14:25:19 +0200

    Get product by ID

--- src/components/home/ProductInfo.tsx
+++ src/components/home/ProductInfo.tsx
@@ -1,31 +1,51 @@
+import * as RB from 'react-bootstrap'
 import { API, graphqlOperation } from 'aws-amplify'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Product } from '../atoms'
+import { getProduct } from '../../graphql/queries'
+import { darkModeState, Product } from '../atoms'
+import { useRecoilValue } from 'recoil'
 
 export default function ProductInfo() {
     const { id } = useParams()
     console.log('This is the id of the product', id)
 
     const [product, updateProduct] = useState<Product>({})
+    const darkMode = useRecoilValue(darkModeState)
+    const check: boolean = darkMode === false 
 
-    async function getProduct() {
+    async function getProductById() {
         try {
             const { data }: any = await API.graphql(graphqlOperation(getProduct, { id: id }))
-            const { items } = data.listProducts
-            updateProduct(items)
+            console.log('This is the data from the product', data)
+            const item = data.getProduct
+            updateProduct(item)
         } catch (error) {
             console.log('Error getting product from DB', error)
         }
     }
 
     useEffect(() => {
-        getProduct()
+        getProductById()
     }, [])
 
     return (
         <div>
-            <h1 className="text-dark">{product.name}</h1>
+            <div className={check ? 'card-holder' : 'card-holderDark'}>
+                <RB.Card onClick={() => toggle(idx)}
+                    className='card'>
+                    <RB.Card.Header className='card-header'
+                        style={{ width: '186px', height: '155px' }}>
+                        <RB.Image className='card-img' src={item.image} alt='' />
+                    </RB.Card.Header>
+                    <RB.Card.Body className='card-body'>
+                        <h6 className='card-text'>{item.name}</h6>
+                        <h6 className='card-text'>{item.description}</h6>
+                        <h6 className='card-text'>{item.price}</h6>
+                        <h6 className='card-text'>{item.category}</h6>
+                    </RB.Card.Body>
+                </RB.Card>
+            </div>
         </div>
     )
 }
