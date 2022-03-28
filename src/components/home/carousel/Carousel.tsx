ccbbb288257ecbae2e55083b17f312f0b7115ccc
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-28 13:55:23 +0200

    commit

--- src/components/home/carousel/Carousel.tsx
+++ src/components/home/carousel/Carousel.tsx
@@ -20,30 +20,6 @@ export default function Carousel({ image }: CarouselProps) {
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </RB.Carousel.Caption>
             </RB.Carousel.Item>
-            {/* <RB.Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                />
-
-                <RB.Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </RB.Carousel.Caption>
-            </RB.Carousel.Item>
-            <RB.Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                />
-
-                <RB.Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </RB.Carousel.Caption>
-            </RB.Carousel.Item> */}
         </RB.Carousel>
     )
 }
